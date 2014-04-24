0f158edbf6a1d626465bc1ce7db7e964d686e722
Philipp Strube <pst@cloudcontrol.de>
2014-04-24 20:53:20 +0200

    Fix company name in license

--- angular-cclib/resources/app.js
+++ angular-cclib/resources/app.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 cloudControl UG (haftungsbeschraenkt)
+ * Copyright 2014 cloudControl GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
